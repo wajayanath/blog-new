445fbbac79996203f6004fa85ee3af2f112a51ef
wajayanath <wajayanath@gmail.com>
2017-02-16 22:14:25 +0530

    blog sample

---
+++ app/Http/Controllers/BlogController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Post;
+use App\Category;
+
+class BlogController extends Controller
+{
+    protected $limit = 3;
+
+    public function index()
+    {
+        $categories = Category::with(['posts' => function($query) {
+            $query->published();
+        }])->orderBy('title', 'asc')->get();
+
+        $posts = Post::with('author')
+                    ->latestFirst()
+                    ->published()
+                    ->simplePaginate($this->limit);
+
+        return view("blog.index", compact('posts', 'categories'));
+    }
+
+    public function category($id)
+    {
+        $categories = Category::with(['posts' => function($query) {
+            $query->published();
+        }])->orderBy('title', 'asc')->get();
+
+        // \DB::enableQueryLog();
+        $posts = Post::with('author')
+                    ->latestFirst()
+                    ->published()
+                    ->where('category_id', $id)
+                    ->simplePaginate($this->limit);
+
+         return view("blog.index", compact('posts', 'categories'));
+
+        //  dd(\DB::getQueryLog());
+    }
+
+    public function show(Post $post)
+    {
+        return view("blog.show", compact('post'));
+    }
+}
