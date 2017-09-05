d6f6579ee9ad35d33f41077c9ecfd812d98a55e4
wajayanath <wajayanath@gmail.com>
2017-09-05 20:46:22 +0530

    redirection

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.main')
+
+@section('content')
+
+<div class="container">
+       <div class="row">
+           <div class="col-md-12">
+               <h2>Page not found</h2>
+           </div>
+       </div>
+   </div>   
+
+@endsection
--- resources/views/layouts/backend/navbar.blade.php
+++ resources/views/layouts/backend/navbar.blade.php
@@ -21,7 +21,7 @@
           <li class="dropdown user user-menu">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
               <img src="/backend/img/user2-160x160.jpg" class="user-image" alt="User Image">
-              <span class="hidden-xs">Alexander Pierce</span>
+              <span class="hidden-xs">{{ Auth::user()->name }}</span>
             </a>
             <ul class="dropdown-menu">
               <!-- User image -->
@@ -29,8 +29,7 @@
                 <img src="/backend/img/user2-160x160.jpg" class="img-circle" alt="User Image">
 
                 <p>
-                  Alexander Pierce - Web Developer
-                  <small>Member since Nov. 2012</small>
+                  {{ Auth::user()->name }} - Web Developer
                 </p>
               </li>
               <!-- Menu Footer-->
