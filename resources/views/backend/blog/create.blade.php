b10602a3eddd98972b13505019ae678cc243fa0c
wajayanath <wajayanath@gmail.com>
2017-11-20 13:05:44 +0530

    form changes

--- resources/views/backend/blog/create.blade.php
+++ resources/views/backend/blog/create.blade.php
@@ -1,125 +1,44 @@
 @extends('layouts.backend.main')
 
-@section('title','MyBlog | Add new post')
+@section('title', 'MyBlog | Add new post')
 
 @section('content')
- <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
-      <h1>
-        Blog
-        <small>Add new post</small>
-      </h1>
-      <ol class="breadcrumb">
-        <li>
-        	<a href="{{ url('/home') }}"><i class="fa fa-dashboard"></i> Dashboard</a>
-        </li>
-        <li><a href="{{ route('backend.blog.index') }}">Blog</a></li>
-        <li class="active">Add New</li>
-      </ol>
-    </section>
 
-    <!-- Main content -->
-    <section class="content">
-        <div class="row">
-          <div class="col-xs-12">
-            <div class="box">
-            	
-              <!-- /.box-header -->
-              <div class="box-body">
-                {!! Form::model($post, [
-                    'method' => 'POST',
-                    'route' => 'backend.blog.store',
-                    'files' => TRUE
-                  ]) !!}
-                  
-                <div class="form-group {{ $errors->has('title') ? 'has-error' : '' }}">
-                  {!! Form::label('title') !!}
-                  {!! Form::text('title', null, ['class' => 'form-control']) !!}
-                  @if ($errors->has('title'))
-                   <span class="help-block">{{ $errors->first('title') }}</span>
-                  @endif
-                </div>
+    <div class="content-wrapper">
+      <!-- Content Header (Page header) -->
+      <section class="content-header">
+        <h1>
+          Blog
+          <small>Add new post</small>
+        </h1>
+        <ol class="breadcrumb">
+          <li>
+              <a href="{{ url('/home') }}"><i class="fa fa-dashboard"></i> Dashboard</a>
+          </li>
+          <li><a href="{{ route('backend.blog.index') }}">Blog</a></li>
+          <li class="active">Add new</li>
+        </ol>
+      </section>
 
-                <div class="form-group {{ $errors->has('slug') ? 'has-error' : '' }}">
-                  {!! Form::label('slug') !!}
-                  {!! Form::text('slug', null, ['class' => 'form-control']) !!}
-                  @if ($errors->has('slug'))
-                   <span class="help-block">{{ $errors->first('slug') }}</span>
-                  @endif
-                </div>
+      <!-- Main content -->
+      <section class="content">
+          <div class="row">
+              {!! Form::model($post, [
+                  'method' => 'POST',
+                  'route'  => 'backend.blog.store',
+                  'files'  => TRUE,
+                  'id' => 'post-form'
+              ]) !!}
 
-                <div class="form-group">
-                  {!! Form::label('excerpt') !!}
-                  {!! Form::textarea('excerpt', null, ['class' => 'form-control']) !!}
-                </div>
+              @include('backend.blog.form')
 
-                <div class="form-group {{ $errors->has('body') ? 'has-error' : '' }}">
-                  {!! Form::label('body') !!}
-                  {!! Form::textarea('body', null, ['class' => 'form-control']) !!}
-                  @if ($errors->has('body'))
-                   <span class="help-block">{{ $errors->first('body') }}</span>
-                  @endif
-                </div>
-
-                 <div class="form-group {{ $errors->has('published_at') ? 'has-error' : '' }}">
-                  {!! Form::label('published_at', 'Published Date') !!}
-                  {!! Form::text('published_at', null, ['class' => 'form-control', 'placeholder' => 'Y-m-d H:i:s']) !!}
-                  @if ($errors->has('published_at'))
-                   <span class="help-block">{{ $errors->first('published_at') }}</span>
-                  @endif
-                </div>
-
-                <div class="form-group {{ $errors->has('category_id') ? 'has-error' : '' }}">
-                  {!! Form::label('category_id', 'Category') !!}
-                  {!! Form::select('category_id', App\Category::pluck('title','id'), null, ['class' => 'form-control', 'placeholder' => 'Choose Category']) !!}
-                  @if ($errors->has('category_id'))
-                   <span class="help-block">{{ $errors->first('category_id') }}</span>
-                  @endif
-                </div>
-
-
-                <div class="form-group {{ $errors->has('image') ? 'has-error' : '' }}">
-                  {!! Form::label('image', 'Feature Image') !!}
-                  {!! Form::file('image') !!}
-                  @if ($errors->has('image'))
-                   <span class="help-block">{{ $errors->first('image') }}</span>
-                  @endif
-                </div>
-
-                <hr>
-                {!! Form::submit('Create new post', ['class' => 'btn btn-primary']) !!}
-
-                {!! Form::close() !!} 
-              </div>
-              <!-- /.box-body -->
-            </div>
-            <!-- /.box -->
+            {!! Form::close() !!}
           </div>
-        </div>
-      <!-- ./row -->
-    </section>
-    <!-- /.content -->
-  </div>
+        <!-- ./row -->
+      </section>
+      <!-- /.content -->
+    </div>
+
 @endsection
 
-@section('script')
-	<script type="text/javascript">
-		$('ul.pagination').addClass('no-margin pagination-sm');
-
-    $('#title').on('blur', function() {
-        var theTitle = this.value.toLowerCase().trim(),
-        slugInput = $('#slug'),
-        theSlug = theTitle.replace(/&/g, '-and-')
-                          .replace(/[^a-z0-9-]+/g, '-')
-                          .replace(/\-\-+/g,'-')
-                          .replace(/^-+|-+$/g, '');
- 
-        slugInput.val(theSlug);
-
-        var simplemde1 = new SimpleMDE({ element: $("#excerpt")[0] });
-        var simplemde2 = new SimpleMDE({ element: $("#body")[0] });
-
-    });
-	</script>
-@endsection
+@include('backend.blog.script')
--- resources/views/layouts/backend/main.blade.php
+++ resources/views/layouts/backend/main.blade.php
@@ -18,8 +18,9 @@
        folder instead of downloading all of them to reduce the load. -->
   <link rel="stylesheet" href="/backend/css/skins/_all-skins.min.css">
   <link rel="stylesheet" href="/backend/plugins/simplemde/simplemde.min.css">
-  <!-- bootstrap wysihtml5 - text editor -->
-  <link rel="stylesheet" href="../../plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.37/css/bootstrap-datetimepicker.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jasny-bootstrap/3.1.3/css/jasny-bootstrap.min.css">
+  <link rel="stylesheet" href="/backend/css/custom.css">
 
   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -30,13 +31,10 @@
 </head>
 <body class="hold-transition skin-blue sidebar-mini">
 <div class="wrapper">
-
   @include('layouts.backend.navbar')
   @include('layouts.backend.sidebar')
 
-    <!-- Content Wrapper. Contains page content -->
     @yield('content')
-    <!-- /.content-wrapper -->
 
   <footer class="main-footer">
     <div class="pull-right hidden-xs">
@@ -54,10 +52,11 @@
 <!-- Bootstrap 3.3.6 -->
 <script src="/backend/js/bootstrap.min.js"></script>
 <script src="/backend/plugins/simplemde/simplemde.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.14.1/moment-with-locales.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.37/js/bootstrap-datetimepicker.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jasny-bootstrap/3.1.3/js/jasny-bootstrap.min.js"></script>
 <!-- AdminLTE App -->
 <script src="/backend/js/app.min.js"></script>
-
   @yield('script')
-  
 </body>
 </html>
